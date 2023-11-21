4e80161f533f1a75848be4d5909ec59a848a9fd0
slorochi <kuro79779@gmail.com>
2023-11-21 02:09:52 -0800

    wip list

--- StilOdot/ContentView.swift
+++ StilOdot/ContentView.swift
@@ -5,11 +5,6 @@
 //  Created by LANDA Théo on 20/11/2023.
 //
 
-//let backgroundUrl = URL(string:"stering")
-//AsyncImage(url:backgroundUrl){
-//    image in image
-//} placeholder:{Rectangle().foregroundColor(.gray)}
-
 
 import SwiftUI
 import UIKit
@@ -19,7 +14,7 @@ struct Team:Identifiable {
     let imageUrl: String
     let name: String
     let role: String
-    let rating: Double
+    let rating: Int
     let salary: Int
     let creationDate: Date
     let color: Color
@@ -28,7 +23,7 @@ struct Team:Identifiable {
 class TeamStore: ObservableObject {
     @Published var members: [Team] = []
     
-    func addMember(imageUrl: String, name: String, role: String, rating: Double,salary:Int, creationDate: Date, color: Color) {
+    func addMember(imageUrl: String, name: String, role: String, rating: Int,salary:Int, creationDate: Date, color: Color) {
         let newTeam = Team(imageUrl: imageUrl, name: name, role: role, rating: rating, salary: salary, creationDate: creationDate, color: color)
         members.append(newTeam)
     }
@@ -41,32 +36,58 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack {
+                HStack {
+                    Text("Votre team")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .bold()
+                    // Couleur du texte du titre
+                    Spacer() // Pour pousser le bouton "+" vers la droite
+                    Button(action: {
+                        isShowingAddTeamView.toggle()
+                    }) {
+                        Image(systemName: "plus")
+                            .foregroundColor(.indigo)
+                            .font(.title3)
+                            .bold()// Couleur de l'icône du bouton "+"
+                    }
+                    .padding([.vertical],7)
+                    .padding([.horizontal],16)
+                    .background(Color.white.opacity(1)) // Couleur de fond du bouton "+"
+                    .cornerRadius(8) // Coins arrondis du bouton "+"
+                }
+                .padding() // Ajout de marges pour espacer du bord de l'écran
                 List {
                     ForEach(teamStore.members) { member in
-                            HStack(alignment: .center, spacing: 0) {
-                                AsyncImage(url: URL(string: "https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg")) { image in
+                        HStack() {
+                            AsyncImage(url: member.imageUrl != "" ? URL(string: member.imageUrl)! : URL(string: "https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg")!) { phase in
+                                switch phase {
+                                case .success(let image):
                                     image
                                         .resizable()
                                         .aspectRatio(contentMode: .fit)
-                                } placeholder: {
+                                case .failure, .empty:
+                                    // En cas d'échec ou si l'image est vide
                                     ProgressView()
+                                @unknown default:
+                                    EmptyView()
                                 }
-                                .frame(width: 70, height: 70)
-                                
-                                VStack(alignment: .leading, spacing: 4) {
-                                    Text(member.name)
-                                        .font(.headline)
-                                        .foregroundColor(.gray)
-                                    HStack(spacing:4){
-                                        Text("Salaire: ")
-                                            .font(.subheadline)
-                                        Text("\(member.salary)€")
-                                            .font(.subheadline)
-                                            .bold()
-                                    }
-                                    
-                                    
+                            }
+                            .frame(width: 120, height: 120)
+                            Spacer()
+                            
+                            VStack(alignment: .leading) {
+                                Text(member.name)
+                                    .font(.headline)
+                                    .foregroundColor(.gray)
+                                HStack(){
+                                    Text("Salaire: ")
+                                        .font(.subheadline)
+                                    Text("\(member.salary)€")
+                                        .font(.subheadline)
+                                        .bold()
                                 }
+                            }
                                 Text(member.role)
                                     .font(.subheadline)
                                 
@@ -79,22 +100,18 @@ struct ContentView: View {
                                         .font(.subheadline)
                                 }
                             }
-                            .padding(2)
+                           
                             .background(member.color)
                             .cornerRadius(8)
                     }
                 }
                 
-                Button("Add Team") {
-                    isShowingAddTeamView.toggle()
-                }
                 .sheet(isPresented: $isShowingAddTeamView) {
                     AddMemberView(teamStore: teamStore)
-                }
+                }.background(Color.indigo.opacity(1))
                 .padding()
-            }
-            .navigationTitle("Gestion d'équipe")
-        }
+            } .background(Color.indigo.opacity(1))
+            } .background(Color.indigo.opacity(1))
     }
     
     func formattedDate(date: Date) -> String {
@@ -109,7 +126,7 @@ struct AddMemberView: View {
     @State private var imageUrl = ""
     @State private var name = ""
     @State private var role = ""
-    @State private var rating = 0.0
+    @State private var rating = 0
     @State private var salary = 0
     @State private var creationDate = Date()
     @State private var color = Color(.white)
@@ -120,7 +137,7 @@ struct AddMemberView: View {
                     TextField("Image URL", text: $imageUrl)
                     TextField("Nom", text: $name)
                     TextField("Role", text: $role)
-                    Stepper(value: $rating, in: 0...10, step: 1) {
+                    Stepper(value: $rating, in: 0...10) {
                         Text("Note: \(rating, specifier: "%.0f")")
                     }
                     HStack {TextField("Salaire", value: $salary , format: .number)
@@ -147,147 +164,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-// ------APP GOMOKU------
-// called by the app
-//struct ContentView: View {
-//
-//    @StateObject var gomokuGame = GomokuGame() // Gestion du jeu Gomoku
-//
-//    var body: some View {
-//        VStack {
-//            GameBoardView(gomokuGame: gomokuGame) // Affiche le plateau de jeu
-//                .frame(width: 280, height: 280) // Taille du plateau de jeu
-//        }
-//        .padding()
-//    }
-//}
-//
-//struct GameBoardView: View {
-//    let boardSize = 19  // Taille de la grille (15x15 pour Gomoku)
-//    let gridSize: CGFloat = 20 // Taille d'une intersection de la grille
-//
-//    @ObservedObject var gomokuGame: GomokuGame // ObservableObject pour mettre à jour la vue
-//
-//    var body: some View {
-//        ZStack {
-//            Image("devilsplan")
-//                .resizable()
-//                .aspectRatio(contentMode: .fill)
-//                .frame(maxWidth: .infinity, maxHeight: .infinity)
-//                .opacity(0.95)
-//                .edgesIgnoringSafeArea(.all) // Ignorer les marges sûres pour remplir tout l'écran
-//            VStack(spacing: 0) {
-//
-//                Text("Gomoku")
-//                    .font(.system(size:36)).foregroundColor(Color.white)
-//                    .padding(20)
-//                    .background(Color.indigo)
-//                Spacer()
-//                ForEach(0..<boardSize, id: \.self) { row in
-//                    HStack(spacing: 0) {
-//                        ForEach(0..<boardSize, id: \.self) { col in
-//
-//                            Rectangle() // Intersection de la grille
-//                                .frame(width: self.gridSize, height: self.gridSize)
-//                                .border(Color.gray) // Ajoute une bordure pour chaque intersection
-//
-//                                .onTapGesture {
-//                                    gomokuGame.makeMove(row: row, col: col)
-//                                }
-//                                .foregroundColor(getPlayerColor(player: gomokuGame.board[row][col])) // Change la couleur du pion
-//                        }
-//                    }
-//                    .background(Color.indigo) // Définir la couleur de fond du plateau de jeu comme noir
-//
-//                }
-//
-//
-//            }
-//        }
-//    }
-//    func getPlayerColor(player: Player) -> Color {
-//            switch player {
-//            case .black:
-//                return .black
-//            case .white:
-//                return .white
-//            default:
-//                return .clear
-//            }
-//        }
-//}
-//
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
-//
-//
-//enum Player {
-//    case none, black, white
-//}
-//
-//class GomokuGame: ObservableObject { // Modifier la classe pour qu'elle soit conforme à ObservableObject
-//    @Published var board: [[Player]]
-//    @Published var currentPlayer: Player
-//
-//    init() {
-//        self.board = Array(repeating: Array(repeating: .none, count: 19), count: 19) // Plateau de 19x19
-//        self.currentPlayer = .black // Joueur noir commence
-//    }
-//
-//    func makeMove(row: Int, col: Int) {
-//        if board[row][col] == .none {
-//            board[row][col] = currentPlayer
-//            // Implémenter la logique de vérification de la victoire ici
-//            // Vérifier si le joueur actuel a gagné
-//            // Sinon, passer au prochain joueur
-//        }
-//    }
-//}
-
-//class GameBoardView: UIView {
-//    let boardSize = 15 // Taille de la grille (15x15 pour Gomoku)
-//    let gridSize = 30 // Taille d'une intersection de la grille
-//
-//    var board: [[Player]] = [] // Matrice pour stocker l'état du jeu
-//    var currentPlayer: Player = .black // Joueur actuel
-//
-//    override func draw(_ rect: CGRect) {
-//        super.draw(rect)
-//        drawBoard() // Dessiner le plateau initial
-//    }
-//
-//    func drawBoard() {
-//        // Dessiner la grille
-//        for i in 0..<boardSize {
-//            for j in 0..<boardSize {
-//                let rect = CGRect(x: CGFloat(i * gridSize), y: CGFloat(j * gridSize), width: CGFloat(gridSize), height: CGFloat(gridSize))
-//                let path = UIBezierPath(rect: rect)
-//                UIColor.lightGray.setFill()
-//                path.fill()
-//                path.stroke()
-//            }
-//        }
-//    }
-
-//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        if let touch = touches.first {
-//            let touchLocation = touch.location(in: self)
-//            let row = Int(touchLocation.x) / gridSize
-//            let col = Int(touchLocation.y) / gridSize
-//
-//            // Assurez-vous que le mouvement est à l'intérieur des limites du plateau
-//            guard row >= 0 && row < boardSize && col >= 0 && col < boardSize else { return }
-//
-//            // Si l'intersection est vide, placez un pion pour le joueur actuel
-//            if board[row][col] == .none {
-//                board[row][col] = currentPlayer
-//                setNeedsDisplay() // Redessiner le plateau avec le nouveau pion
-//                // Ici, vous pouvez ajouter la logique pour changer de joueur
-//            }
-//        }
-//    }
-// }
